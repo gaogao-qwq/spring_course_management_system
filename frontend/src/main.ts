6d6eedbdb81f6303166415fd3a75bb7abae40579
gaogao-qwq <gaogaoqwq@gmail.com>
2023-05-24 15:52:25 +0800

    (Frontend) Chore: Clear auto-generated codes

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,11 +1,24 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
-import App from './App.vue'
+
+import 'element-plus/dist/index.css'
+
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
 import router from './router'
+import App from './App.vue'
+import ElementPlus from 'element-plus'
+
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 
 const app = createApp(App)
 
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+
 app.use(router)
+app.use(ElementPlus, {
+  locale: zhCn
+})
 
 app.mount('#app')
