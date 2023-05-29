e0ff120f562d94eb9f4387e6956874f58a99c3ad
gaogao-qwq <gaogaoqwq@gmail.com>
2023-05-29 18:09:09 +0800

    (Backend) Fix: Enable cross site request

---
+++ backend/src/main/java/com/gaogaoqwq/course_info_management_system_backend/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.gaogaoqwq.course_info_management_system_backend.config;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NotNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("*")
+                        .allowCredentials(true)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH");
+            }
+        };
+    }
+
+}
