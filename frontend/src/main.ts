80525588b7673ac5a1b073677811751b3387131b
gaogao-qwq <gaogaoqwq@gmail.com>
2023-06-07 20:32:45 +0800

    (Frontend) Chore: Store token to cookie

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import VueCookies from "vue-cookies"
 
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
@@ -11,10 +12,11 @@ import 'element-plus/dist/index.css'
 
 const app = createApp(App);
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-  app.component(key, component)
+  app.component(key, component);
 }
 
-app.use(createPinia());
-app.use(router);
+app.use(createPinia())
+   .use(VueCookies, {expires: '1h', path: '/'})
+   .use(router);
 
 app.mount("#app");
