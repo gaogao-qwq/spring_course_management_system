9964913c7cba5fbdf41ee06fe8eb25b971565f2c
gaogao-qwq <gaogaoqwq@gmail.com>
2023-06-07 16:56:04 +0800

    (Backend) Update: Add updateUser implementation

--- backend/src/main/java/com/gaogaoqwq/course_info_management_system_backend/controller/user/UserinfoController.java
+++ backend/src/main/java/com/gaogaoqwq/course_info_management_system_backend/controller/user/UserinfoController.java
@@ -1,28 +1,53 @@
 package com.gaogaoqwq.course_info_management_system_backend.controller.user;
 
+import com.gaogaoqwq.course_info_management_system_backend.entity.user.User;
+import com.gaogaoqwq.course_info_management_system_backend.repository.user.UserRepository;
 import com.gaogaoqwq.course_info_management_system_backend.response.R;
+import com.gaogaoqwq.course_info_management_system_backend.response.ResultCode;
+import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
-import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
 
-@RestController()
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/user")
 public class UserinfoController {
-    @GetMapping("/me")
-    public R currentUser(@NotNull @AuthenticationPrincipal UserDetails userDetails){
+    private final AuthenticationManager authenticationManager;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @GetMapping("/")
+    public R currentUser(@NotNull @AuthenticationPrincipal UserDetails userDetails) {
         Map<Object, Object> model = new HashMap<>();
         model.put("username", userDetails.getUsername());
         model.put("roles", userDetails.getAuthorities().stream()
                 .map(GrantedAuthority::getAuthority)
-                .collect(Collectors.toList())
+                .toList()
         );
         return R.success().data(model);
+    }
+
+    @PostMapping("/update")
+    public R updateUser(@RequestBody @NotNull Map<String, String> request)
+            throws RuntimeException {
+        String username = request.get("username");
+        String oldPassword = request.get("old_password");
+        String newPassword = request.get("new_password");
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(username, oldPassword));
+        User user = userRepository.findUserByUsername(username).orElseThrow();
+        user.setPassword(passwordEncoder.encode(newPassword));
+        User entity = userRepository.save(user);
+        return R.success().code(ResultCode.SUCCESS.getCode()).data(entity);
     }
 }
