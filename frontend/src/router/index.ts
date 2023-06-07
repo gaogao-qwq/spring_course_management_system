f688e8bd8971912d02582118c71a76012e2a5be1
gaogao-qwq <gaogaoqwq@gmail.com>
2023-06-07 20:33:08 +0800

    (Frontend) Update: Update router

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,26 +2,51 @@ import profileView from "@/views/ProfileView.vue";
 import adminView from "@/views/admin/AdminView.vue"
 import HomeView from "@/views/HomeView.vue";
 import { createRouter, createWebHistory } from "vue-router";
+import StudentView from "@/views/student/StudentView.vue";
+import TeacherView from "@/views/teacher/TeacherView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
-      name: "home",
       component: HomeView,
     },
     {
-      path: "/profile",
-      name: "profile",
-      component: profileView,
+      path: "/admin",
+      component: adminView,
+      children: [
+        {
+          path: "profile",
+          component: profileView,
+        }
+      ]
     },
     {
-      path: "/admin",
-      name: "admin",
-      component: adminView,
+      path: "/teacher",
+      component: TeacherView,
+      children: [
+        {
+          path: "profile",
+          component: profileView,
+        }
+      ]
+    },
+    {
+      path: "/student",
+      component: StudentView,
+      children: [
+        {
+          path: "profile",
+          component: profileView,
+        }
+      ]
     }
   ],
 });
+
+router.beforeEach((to) => {
+  
+})
 
 export default router;
