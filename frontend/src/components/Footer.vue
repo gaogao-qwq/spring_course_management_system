afa075207ec5391d42e74a913752a897eaecb9b5
gaogao-qwq <gaogaoqwq@gmail.com>
2023-06-11 19:50:04 +0800

    (Frontend) Update: Update footer

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,7 +1,28 @@
 <script setup lang="ts">
+import { auto } from '@popperjs/core';
+
 </script>
 
-<template></template>
+<template>
+  <div class="footer">
+    <el-space direction="vertical">
+      <el-text>
+        Release under the 
+        <el-link href="https://www.gnu.org/licenses/gpl-3.0.en.html">GPL-3.0 license</el-link>
+      </el-text>
+      <el-text>
+        Made with ❤️ by 
+        <el-link href="https://github.com/gaogao-qwq">gaogao_qwq</el-link>
+      </el-text>
+    </el-space>
+  </div>
+</template>
 
-<script scoped>
-</script>
+<style scoped>
+.footer {
+  position: relative;
+  height: auto;
+  box-sizing: border-box;
+  text-align: center;
+}
+</style>
