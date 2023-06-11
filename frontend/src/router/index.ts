768c2fbd88b6fcb6205135a05f2475a674d849f3
gaogao-qwq <gaogaoqwq@gmail.com>
2023-06-11 19:48:17 +0800

    (Frontend) Update: Update router

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,30 +1,65 @@
 import profileView from "@/views/ProfileView.vue";
 import adminView from "@/views/admin/AdminView.vue"
-import HomeView from "@/views/HomeView.vue";
+import homeView from "@/views/HomeView.vue";
 import { createRouter, createWebHistory } from "vue-router";
-import StudentView from "@/views/student/StudentView.vue";
-import TeacherView from "@/views/teacher/TeacherView.vue";
+import studentView from "@/views/student/StudentView.vue";
+import teacherView from "@/views/teacher/TeacherView.vue";
+import studentManagementView from "@/views/admin/StudentManagementView.vue";
+import adminHomeView from "@/views/admin/AdminHomeView.vue";
+import classManagementView from "@/views/admin/ClassManagementView.vue";
+import majorManagementView from "@/views/admin/MajorManagementView.vue";
+import courseManagementView from "@/views/admin/CourseManagementView.vue";
+import userManagementView from "@/views/admin/UserManagementView.vue";
+import teacherManagementView from "@/views/admin/TeacherManagementView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
-      component: HomeView,
+      component: homeView,
     },
     {
       path: "/admin",
       component: adminView,
       children: [
         {
+          path: "home",
+          component: adminHomeView
+        },
+        {
+          path: "student",
+          component: studentManagementView
+        },
+        {
+          path: "class",
+          component: classManagementView
+        },
+        {
+          path: "teacher",
+          component: teacherManagementView
+        },
+        {
+          path: "major",
+          component: majorManagementView
+        },
+        {
+          path: "course",
+          component: courseManagementView
+        },
+        {
+          path: "user",
+          component: userManagementView
+        },
+        {
           path: "profile",
-          component: profileView,
+          component: profileView
         }
       ]
     },
     {
       path: "/teacher",
-      component: TeacherView,
+      component: teacherView,
       children: [
         {
           path: "profile",
@@ -34,7 +69,7 @@ const router = createRouter({
     },
     {
       path: "/student",
-      component: StudentView,
+      component: studentView,
       children: [
         {
           path: "profile",
