f980dea0c99b640da34dd385bed4db60bc14cea6
gaogao-qwq <gaogaoqwq@gmail.com>
2023-06-11 19:50:37 +0800

    (Frontend) Fix: Fix bad profile info handle logic

--- frontend/src/views/ProfileView.vue
+++ frontend/src/views/ProfileView.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 import { UserInfoApi } from '@/internal/apis';
-import type { FormRules } from 'element-plus';
-import { isEmpty, isNull, isString } from 'lodash';
-import { reactive } from 'vue';
+import type { FormInstance, FormRules } from 'element-plus';
+import { isEmpty, isNull, isString, isUndefined } from 'lodash';
+import { reactive, ref } from 'vue';
 
 let resp = await UserInfoApi
-  .get('/user/')
+  .get('/user/me')
   .then((response) => {
     if(!response.data.success || isNull(response.data.data)) {
       return response.data.message
@@ -20,6 +20,7 @@ if (isString(resp)) {
 }
 
 const username = resp.username
+const ruleFormRef = ref<FormInstance>()
 const form = reactive({
   username: resp.username,
   old_password: "",
@@ -49,35 +50,52 @@ const rules = reactive<FormRules>({
   new_password: { validator: passwordValidator },
 })
 
-const onSubmit = async () => {
-  
+const onSubmit = async (formEl: FormInstance | undefined) => {
+  if (isUndefined(formEl) || isNull(formEl)) return
+
+  formEl?.validate(async (valid) => {
+    if (!valid) {
+      return;
+    }
+  });
 }
 </script>
 
 <template>
   <el-card class="profile-card">
-    <el-form :rules="rules">
+    <el-form 
+      :model="form"
+      :rules="rules"
+      ref="ruleFormRef"
+      status-icon
+    >
       <el-form-item label="账号" prop="username">
         <el-input v-model="form.username" disabled />
       </el-form-item>
       <el-form-item label="旧密码" prop="old_password">
         <el-input 
+          type="password"
           v-model="form.old_password"
-          max-length="20"
+          minlength="4"
+          maxlength="20"
           placeholder="输入旧密码" 
+          show-password
         />
       </el-form-item>
       <el-form-item label="新密码" prop="new_password">
         <el-input
+          type="password"
           v-model="form.new_password"
-          max-length="20"
+          minlength="4"
+          maxlength="20"
           placeholder="输入新密码"
+          show-password
         />
       </el-form-item>
       <el-row justify="center">
         <el-button
           type="primary"
-          @click=onSubmit
+          @click=onSubmit(ruleFormRef)
         >
           提交
         </el-button>
@@ -88,6 +106,7 @@ const onSubmit = async () => {
 
 <style scoped>
 .profile-card {
-  position: relative;
+  border-radius: 8px;
+  margin: 10px;
 }
 </style>
