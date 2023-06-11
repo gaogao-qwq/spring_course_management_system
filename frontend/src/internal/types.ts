f731de2ca0bda1fa7a61dcf73577409ae6aee202
gaogao-qwq <gaogaoqwq@gmail.com>
2023-06-11 19:47:17 +0800

    (Frontend) Update: Update type interface

---
+++ frontend/src/internal/types.ts
@@ -0,0 +1,45 @@
+export interface Student {
+  id: number
+  codeName: string
+  name: string
+  gender: number
+  birthDate: string
+  admissionDate: string
+  fkClass: Class
+}
+
+export interface Teacher {
+  id: number
+  codeNmae: string
+  name: string
+  gender: number
+  birthDate: string
+  admissionDate: string
+}
+
+export interface Class {
+  id: number
+  codeNmae: string
+  name: string
+  fkMajor: Major
+}
+
+export interface Major {
+  id: number
+  codeNmae: string
+  name: string
+}
+
+export interface Course {
+  id: number
+  codeNmae: string
+  name: string
+  fkTeacher: Teacher
+  fkClass: Class
+}
+
+export interface User {
+  id: number
+  username: string
+  role: any[]
+}
