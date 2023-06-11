dad1ed7176002c392ebe9c45fe89611591f08ef2
gaogao-qwq <gaogaoqwq@gmail.com>
2023-06-11 19:47:35 +0800

    (Frontend) Update: Update apis

--- frontend/src/internal/apis.ts
+++ frontend/src/internal/apis.ts
@@ -8,11 +8,83 @@ export const LoginApi = axios.create({
 })
 
 export const UserInfoApi = axios.create({
-  url: '/user/',
+  url: '/user/me',
 })
 
 export const UpdateUserInfoApi = axios.create({
   url: '/user/update',
+})
+
+export const GetUserCountApi = axios.create({
+  url: '/user/count'
+})
+
+export const FetchUserApi = axios.create({
+  url: '/user/all'
+})
+
+export const FetchUserByPageApi = axios.create({
+  url: '/user/'
+})
+
+export const FetchStudentApi = axios.create({
+  url: '/student/all'
+})
+
+export const FetchStudentByPageApi = axios.create({
+  url: '/student/'
+})
+
+export const GetStudentCountApi = axios.create({
+  url: '/student/count'
+})
+
+export const FetchClassApi = axios.create({
+  url: '/class/all'
+})
+
+export const FetchClassByPageApi = axios.create({
+  url: '/class/'
+})
+
+export const GetClassCountApi = axios.create({
+  url: '/class/count'
+})
+
+export const FetchTeacherApi = axios.create({
+  url: '/teacher/all'
+})
+
+export const FetchTeacherByPageApi = axios.create({
+  url: '/teacher/'
+})
+
+export const GetTeacherCountApi = axios.create({
+  url: '/teacher/count'
+})
+
+export const FetchMajorApi = axios.create({
+  url: '/major/all'
+})
+
+export const FetchMajorByPageApi = axios.create({
+  url: '/major/'
+})
+
+export const GetMajorCountApi = axios.create({
+  url: '/major/count'
+})
+
+export const FetchCourseApi = axios.create({
+  url: '/course/all'
+})
+
+export const FetchCourseByPageApi = axios.create({
+  url: '/course/'
+})
+
+export const GetCourseCountApi = axios.create({
+  url: '/course/count'
 })
 
 
