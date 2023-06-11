620b05a6a665a5153b0a30b340d9a204f078b739
gaogao-qwq <gaogaoqwq@gmail.com>
2023-06-11 19:45:56 +0800

    (Frontend) Chore: Set element-plus locale to zh-cn

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import VueCookies from "vue-cookies"
+import ElementPlus from 'element-plus'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
@@ -15,7 +17,8 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component);
 }
 
-app.use(createPinia())
+app.use(ElementPlus, { locale: zhCn })
+   .use(createPinia())
    .use(VueCookies, {expires: '1h', path: '/'})
    .use(router);
 
