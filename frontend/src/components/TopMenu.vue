332a6ffba51bc6b0ce0d5922ff48ea4de812d4a0
gaogao-qwq <gaogaoqwq@gmail.com>
2023-06-13 13:11:00 +0800

    (Frontend) Perf: Add more info in userinfo

--- frontend/src/components/TopMenu.vue
+++ frontend/src/components/TopMenu.vue
@@ -8,7 +8,7 @@ const props = defineProps({
 })
 
 const circleAvatarUrl = 'https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png'
-const $cookies = inject<VueCookies>('$cookies');
+const $cookies = inject<VueCookies>('$cookies') as VueCookies;
 
 const handleRouting = (path: string) => {
   router.push(path)
@@ -20,13 +20,24 @@ const handleLogout = () => {
   handleRouting("/")
 }
 
+const userInfo = computed(() =>{
+  let date = new Date()
+  if (date.getHours() < 12) {
+    return `早上好，${$cookies.get('username')}`
+  }
+  if (date.getHours() < 18) {
+    return `下午好，${$cookies.get('username')}`
+  }
+  return `晚上好，${$cookies.get('username')}`
+})
+
 </script>
 
 <template>
   <div class="toolbar">
     <el-text size="large">教务管理系统</el-text>
     <div class="flex-grow" />
-    <el-text>{{ $cookies.get('username') }}</el-text>
+    <el-text>{{ userInfo }}</el-text>
     <el-dropdown>
       <el-icon style="margin-left: 8px; margin-top: 1px">
         <setting/>
