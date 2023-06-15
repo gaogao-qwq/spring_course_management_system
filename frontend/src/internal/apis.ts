ec0bb6dd2be836035822798b27b5379140497eeb
gaogao-qwq <gaogaoqwq@gmail.com>
2023-06-15 09:46:36 +0800

    (Frontend) Perf: Add timeout to each api request

--- frontend/src/internal/apis.ts
+++ frontend/src/internal/apis.ts
@@ -5,86 +5,107 @@ axios.defaults.withCredentials = true
 
 export const LoginApi = axios.create({
   url: '/auth/login',
+  timeout: 5000
 })
 
 export const UserInfoApi = axios.create({
   url: '/user/me',
+  timeout: 5000
 })
 
 export const UpdateUserInfoApi = axios.create({
   url: '/user/update',
+  timeout: 5000
 })
 
 export const GetUserCountApi = axios.create({
-  url: '/user/count'
+  url: '/user/count',
+  timeout: 5000
 })
 
 export const FetchUserApi = axios.create({
-  url: '/user/all'
+  url: '/user/all',
+  timeout: 5000
 })
 
 export const FetchUserByPageApi = axios.create({
-  url: '/user/'
+  url: '/user/',
+  timeout: 5000
 })
 
 export const FetchStudentApi = axios.create({
-  url: '/student/all'
+  url: '/student/all',
+  timeout: 5000
 })
 
 export const FetchStudentByPageApi = axios.create({
-  url: '/student/'
+  url: '/student/',
+  timeout: 5000
 })
 
 export const GetStudentCountApi = axios.create({
-  url: '/student/count'
+  url: '/student/count',
+  timeout: 5000
 })
 
 export const FetchClassApi = axios.create({
-  url: '/class/all'
+  url: '/class/all',
+  timeout: 5000
 })
 
 export const FetchClassByPageApi = axios.create({
-  url: '/class/'
+  url: '/class/',
+  timeout: 5000
 })
 
 export const GetClassCountApi = axios.create({
-  url: '/class/count'
+  url: '/class/count',
+  timeout: 5000
 })
 
 export const FetchTeacherApi = axios.create({
-  url: '/teacher/all'
+  url: '/teacher/all',
+  timeout: 5000
 })
 
 export const FetchTeacherByPageApi = axios.create({
-  url: '/teacher/'
+  url: '/teacher/',
+  timeout: 5000
 })
 
 export const GetTeacherCountApi = axios.create({
-  url: '/teacher/count'
+  url: '/teacher/count',
+  timeout: 5000
 })
 
 export const FetchMajorApi = axios.create({
-  url: '/major/all'
+  url: '/major/all',
+  timeout: 5000
 })
 
 export const FetchMajorByPageApi = axios.create({
-  url: '/major/'
+  url: '/major/',
+  timeout: 5000
 })
 
 export const GetMajorCountApi = axios.create({
-  url: '/major/count'
+  url: '/major/count',
+  timeout: 5000
 })
 
 export const FetchCourseApi = axios.create({
-  url: '/course/all'
+  url: '/course/all',
+  timeout: 5000
 })
 
 export const FetchCourseByPageApi = axios.create({
-  url: '/course/'
+  url: '/course/',
+  timeout: 5000
 })
 
 export const GetCourseCountApi = axios.create({
-  url: '/course/count'
+  url: '/course/count',
+  timeout: 5000
 })
 
 
